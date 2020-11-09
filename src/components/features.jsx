4de54a35156c9559a11b5a9be7a482f85bdbee2b
Julio García Pérez <julio.garciaperez@telefonica.com>
2020-11-09 22:45:58 +0100

    feat: add logos

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -5,6 +5,15 @@ export class features extends Component {
     return (
       <div id="features" className="text-center">
         <div className="container">
+          <div className="row logos">
+            <div className="col-12 col-md-6">
+              <img src="/img/logo.jpeg" alt="logo"/>
+            </div>
+            <div className="col-12 col-md-6">
+              <img src="/img/logo1.jpeg" alt="logo"/>
+            </div>
+          </div>
+          
           <div className="col-md-10 col-md-offset-1 section-title">
             <h2>Servicios</h2>
           </div>
